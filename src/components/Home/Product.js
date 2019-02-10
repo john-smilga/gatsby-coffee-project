8d4d54fd70cfa65b0f99b15c41e02b2c9c464d7a
John <johnsmilga@gmail.com>
2019-02-10 15:01:26 -0800

    Testing Simple Forward Slash for Url

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -15,7 +15,7 @@ export default function Product({ product }) {
               data-item-name={product.title}
               data-item-price={product.price}
               data-item-image={product.image.fluid.src}
-              data-item-url="https://gatsby-coffee-project.netlify.com/"
+              data-item-url="/"
             >
               add to cart
             </button>
